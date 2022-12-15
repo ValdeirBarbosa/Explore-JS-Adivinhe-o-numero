6f126f6f5f09abd7b3e07a09ac5da42bad288965
Valdeir Barbosa <valdeir.barbosa@outlook.com>
2022-12-14 22:37:03 -0300

    Update script.js
    refatorando funções e validando inputs

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
-const randNum = Math.round(Math.random()*10);
-console.log(randNum)
-
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+let randNum = Math.round(Math.random() * 10);
 let xAttemps = 1;
 
+let screen1 = document.querySelector(".screen1");
+let screen2 = document.querySelector(".screen2");
 
-function handleTryClick(event){
-    event.preventDefault()
-    const inputNumber = document.querySelector("#inputNumber");
-    console.log(inputNumber.value)
+//Eventos
 
-    if(Number(inputNumber.value) == randNum){
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
-        document.querySelector(".screen2 h2").innerText = stringfy(xAttemps)
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+
+document.addEventListener('keydown',handleKeyPress);
+
+//Callbacks
+
+function handleKeyPress(event){
+    if(event.key='Enter' && screen1.classList.contains("hide")){
+        handleResetClick()
     }
-    inputNumber.value = ""
-    
-    xAttemps++
+
+}
+function handleTryClick(event) {
+  event.preventDefault();
+  const inputNumber = document.querySelector("#inputNumber");
+  
+  console.log(inputNumber.value);
+  if(inputNumber.value ==""){
+    alert("Campo vazio, nao valeu!")
+  }else if(Number(inputNumber.value) <=10 && Number(inputNumber.value) >=0 ){
+    if (Number(inputNumber.value) == randNum) {
+        toggleScreen();
+        document.querySelector(".screen2 h2").innerText = stringfy(xAttemps);
+      }
+      xAttemps++;
+  }else{
+    alert("O numero deve ser entre 0 e 10!")
+  }
+  inputNumber.value = "";
 }
 
-function stringfy(attemps){
-    switch(attemps){
-        case 1:
-            return `Você consegiu em ${attemps} tentativa!`
-            break
-        default:
-            return `Você consegiu em ${attemps} tentativas!`
-    }
+function stringfy(attemps) {
+  switch (attemps) {
+    case 1:
+      return `Você consegiu em ${attemps} tentativa!`;
+      break;
+    default:
+      return `Você consegiu em ${attemps} tentativas!`;
+  }
 }
 
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
+function handleResetClick() {
+  toggleScreen();
+  xAttemps = 1;
+}
 
-btnTry.addEventListener('click',handleTryClick)
-btnReset.addEventListener('click',function(){
-    document.querySelector(".screen1").classList.toggle("hide")
-    document.querySelector(".screen2").classList.toggle("hide")
-    randNum = Math.round(Math.random()*10);
-    xAttemps = 1
-
-})
+function toggleScreen() {
+  screen2.classList.toggle("hide");
+  screen1.classList.toggle("hide");
+  randNum = Math.round(Math.random() * 10);
+}
